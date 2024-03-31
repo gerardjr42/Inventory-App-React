2797a4cab50c1b4d5c66b04f6315ab120cd99f0b
gerardjr42 <ggarciajr12@gmail.com>
2024-03-31 17:58:44 -0400

    create CustomLink to navigate pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,6 @@ import arrow from '../images/arrow.png'
 import logo from '../images/logo.png'
 
 const SideBar = () => {
-  const path = window.location.pathname
   return (
     <nav className="sidebar close">
       <header>
@@ -19,51 +18,37 @@ const SideBar = () => {
       <div className="menu-bar">
         <div className="menu">
           <ul className="menu-links">
-            {/* Added our CustomLinks */}
-            <li className="nav-links">
-              {/* Add link to main here */}
-              <a href="/main">
-                <i className='bx bx-home-alt sidebar-icons'></i>
-                <span className="nav-text">Home</span>
-              </a>
-            </li>
-            <li className="nav-links">
-              {/* Add link to form here */}
-              <a href="/form"> 
-                <i className='bx bx-book-add sidebar-icons' ></i>
-                <span className="nav-text">Add New Product</span>
-                </a>
-            </li>
-            <li className="nav-links">
-              {/* Add link to table here */}
-              <a href="/table">
-                <i className='bx bx-bar-chart-alt-2 sidebar-icons'></i>
-                <span className="nav-text">Inventory</span>
-              </a>
-            </li>
+            {/* Added our CustomLinks to navigate pages*/}
+            <CustomLink href="/main">
+              <i className='bx bx-home-alt sidebar-icons'></i>
+              <span className="nav-text">Home</span>
+            </CustomLink>
+            
+            <CustomLink href="/form">
+              <i className='bx bx-book-add sidebar-icons' ></i>
+              <span className="nav-text">Add New Product</span>
+            </CustomLink>
+
+            <CustomLink href="/table">
+              <i className='bx bx-bar-chart-alt-2 sidebar-icons'></i>
+              <span className="nav-text">Inventory Table</span>
+            </CustomLink>
           </ul>
         </div>
 
         <div className="bottom-content">
           <ul className="menu-links">
-          <li className="nav-links">
-            <a href="#">
-              <i className='bx bx-log-out sidebar-icons'></i>
-              <span className="nav-text">Logout</span>
-            </a>
-          </li>
+            <li className="mode">
+              <div className="moon-sun">
+                <i className="bx bx-moon sidebar-icons moon"></i>
+                <i className="bx bx-sun sidebar-icons sun"></i>
+              </div>
+              <span className="mode-text text">Dark Mode</span>
 
-          <li className="mode">
-            <div className="moon-sun">
-              <i className="bx bx-moon sidebar-icons moon"></i>
-              <i className="bx bx-sun sidebar-icons sun"></i>
-            </div>
-            <span className="mode-text text">Dark Mode</span>
-
-            <div className="toggle-switch">
-              <span className="switch"></span>
-            </div>
-          </li>
+              <div className="toggle-switch">
+                <span className="switch"></span>
+              </div>
+            </li>
           </ul>
         </div>
       </div>
@@ -72,10 +57,12 @@ const SideBar = () => {
 }
 
 function CustomLink({href, children, ...props}) {
+  const path = window.location.pathname
   return (
-    <li>
-      {/* Add link to main here */}
-      <a href={href}>{children}</a>
+    <li className={`${path === href ? "active" : ""}` + " nav-links"}>
+      <a href={href} {...props}>
+        {children}
+      </a>
     </li>
   )
 }
